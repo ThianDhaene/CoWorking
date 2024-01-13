bd0acda65f697796d469738e044f1c398f54ede7
VermoereL <lukas.vermoere@proton.me>
2024-01-13 18:56:21 +0100

    spinner sound gefixt

--- website/spinner/script.js
+++ website/spinner/script.js
@@ -1,23 +1,32 @@
-let Spinner = new Audio('../../website/img/spinner.mp3');
-function rotateFunction(){
+let Spinner = new Audio('../img/spinner.mp3');
+Spinner.addEventListener('canplaythrough', function () {
+});
+
+function rotateFunction() {
   Spinner.play();
+
   var min = 1024;
   var max = 9999;
   var deg = Math.floor(Math.random() * (max - min)) + min;
-  document.getElementById('box').style.transform = "rotate("+deg+"deg)";
+
+  document.getElementById('box').style.transform = "rotate(" + deg + "deg)";
   var element = document.getElementById('mainbox');
   element.classList.remove('animate');
-  setTimeout(function(){
-    element.classList.add('animate');
-  }, 5000)
+  
   setTimeout(function () {
-    // Create a new paragraph element
+    element.classList.add('animate');
+  }, 5000);
+
+  setTimeout(function () {
     var newTextElement = document.createElement('p');
     newTextElement.textContent = 'Congratulations! You won something!';
     newTextElement.id = 'resultText';
-    // Append the new element to the body
     document.body.appendChild(newTextElement);
   }, 12000);
-  Spinner.pause();
-  Spinner.load();
+
+  // Pause and reload the audio after the congratulations message
+  setTimeout(function () {
+    Spinner.pause();
+    Spinner.load();
+  }, 12000);
 }
